0095b39b6e4f9a06008b1de0adaf785b5db61f61
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-02-19 17:45:21 +0100

    Correction fautes de frappe

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
 let score = 0
 
-let motUtilisateur = prompt("Entrez le mot : " + listeMOts[0])
+let motUtilisateur = prompt("Entrez le mot : " + listeMots[0])
 if (motUtilisateur === listeMots[0]) {
     score++
 }
 
-motUtilisateur = prompt("Entrez le mot : " + listeMOts[1])
+motUtilisateur = prompt("Entrez le mot : " + listeMots[1])
 if (motUtilisateur === listeMots[1]) {
     score++
 }
 
-motUtilisateur = prompt("Entrez le mot : " + listeMOts[2])
+motUtilisateur = prompt("Entrez le mot : " + listeMots[2])
 if (motUtilisateur === listeMots[2]) {
     score++
 }
