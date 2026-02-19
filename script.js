3365f807d803b6c9b25982c00d160c5843a56365
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-02-19 17:26:28 +0100

    pop up + demander de taper "bonjour" comme mot

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello")
+const motApplication = "Bonjour" // Essayez de mettre un autre mot ici !
+let motUtilisateur = prompt("Entrez le mot : " + motApplication)
+
+if (motUtilisateur === motApplication) {
+    console.log("Bravo !")
+} else {
+    console.log("Vous avez fait une erreur de frappe.")
+}
