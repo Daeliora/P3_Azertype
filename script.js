a3f9a535eeac206213aedc288d9a5ff92a34cce3
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-02-19 18:28:09 +0100

    Exo corrigé partie2_3

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique", "La vie, l'univers et le reste", "Merci pour le poisson"]
+
 let score = 0
 
-let motUtilisateur = prompt("Entrez le mot : " + listeMots[0])
-if (motUtilisateur === listeMots[0]) {
-    score++
+let choix = prompt("Avec quelle liste voulez-vous jouer : mots ou phrases ?")
+while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Avec quelle listte voulez-vous jouer : mots ou phrases ?")
 }
 
-motUtilisateur = prompt("Entrez le mot : " + listeMots[1])
-if (motUtilisateur === listeMots[1]) {
-    score++
+if (choix === "mots") {
+    for (let i = 0; i < listeMots.length; i++) {
+        let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+            if (motUtilisateur === listeMots[i]) {
+            score++
+        }
+    }
+console.log("Votre score est de" + score + "sur" + listeMots.length)   
+
+} else {
+    for (let i = 0; i < listePhrases.length; i++) {
+        let motUtilisateur = prompt("Entrez le mot : " + listePhrases[i])
+        if (motUtilisateur === listePhrases[i]) {
+        score++
+}
+    }
+console.log("Votre score est de " + score + " sur " + listePhrases.length)
 }
 
-motUtilisateur = prompt("Entrez le mot : " + listeMots[2])
-if (motUtilisateur === listeMots[2]) {
-    score++
-}
-
-console.log("Votre score est de " + score + " sur 3")
