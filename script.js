e53b4307efa2a1193367c8264dd64e8829432cb3
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-02-19 18:03:07 +0100

    mise en place pour Exo Partie2_3

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
-console.log(listeMots[0])
-console.log(listeMots[1])
-console.log(listeMots[2])
 let score = 0
 
 let motUtilisateur = prompt("Entrez le mot : " + listeMots[0])
@@ -19,10 +16,4 @@ if (motUtilisateur === listeMots[2]) {
     score++
 }
 
-console.log(score)
-
-let compteur = 0
-while (compteur < 3) {
-    console.log(compteur)
-    i++
-}
+console.log("Votre score est de " + score + " sur 3")
