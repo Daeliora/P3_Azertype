454f6739b205c236f0651946de1510170b13e3a5
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-02-24 16:32:34 +0100

    Exo P4_4 diviser la fonction lancerJeu en plusieurs fonctions et utiliser des try/catch et throw

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,9 @@ function afficherResultat(score, nbMotsProposes) {
 
 /**
  * Cette fonction affiche une proposition, que le joueur devra recopier, 
- * dans la zone "zoneProposition"*/
+ * dans la zone "zoneProposition"
+* @param {string} proposition : la proposition à afficher
+ */
 function afficherProposition(proposition) {
     let zoneProposition = document.querySelector(".zoneProposition")
     zoneProposition.innerText = proposition
@@ -39,19 +41,66 @@ function afficherEmail(nom, email, score) {
     location.href = mailto
 }
 
+/**
+ * Cette fonction prend un nom en paramètre et valide qu'il est au bon format
+ * ici : deux caractères au minimum
+ * @param {string} nom 
+ * @throws {Error}
+ */
 function validerNom(nom) {
-    if (nom.lenght >= 2) {
-        return true
+    if (nom.lenght < 2) {
+        throw new Error("Le nom est trop court.")
     }
-        return false
+      
 }
 
+/**
+ * Cette fonction prend un email en paramètre et valide qu'il est au bon format. 
+ * @param {string} email 
+ * @throws {Error}
+ */
 function validerEmail(email) {
     let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
-    if (emailRegExp.test(email)) {
-        return true
+    if (!emailRegExp.test(email)) {
+        throw new Error("L'email n'est pas valide.")
     }
-    return false
+    
+}
+
+
+function afficherMessageErreur(message) {
+    
+    let spanErreurMessage = document.getElementById("erreurMessage")
+
+    if (!spanErreurMessage) {
+        let popup = document.querySelector(".popup")
+
+        spanErreurMessage = document.createElement("span")
+        spanErreurMessage.id = "erreurMessage"
+        
+        popup.append(spanErreurMessage)
+    }
+    
+    spanErreurMessage.innerText = message
+}
+
+
+function gererFormulaire(scoreEmail) {
+    try {
+        let baliseNom = document.getElementById("nom")
+        let nom = baliseNom.value
+        validerNom(nom)
+
+        let baliseEmail = document.getElementById("email")
+        let email = baliseEmail.value
+        validerEmail(email)
+        afficherMessageErreur("")
+        afficherEmail(nom, email, scoreEmail)
+
+    } catch(erreur) {
+        afficherMessageErreur(erreur.message)
+    }
+    
 }
 
 
@@ -106,22 +155,12 @@ function lancerJeu() {
         })
     }
 
+     // Gestion de l'événement submit sur le formulaire de partage. 
     let form = document.querySelector("form")
     form.addEventListener("submit", (event) => {
         event.preventDefault()
-
-        let baliseNom = document.getElementById("nom")
-        let nom = baliseNom.value
-
-        let baliseEmail = document.getElementById("email")
-        let email = baliseEmail.value
-
-        if (validerNom(nom) && validerEmail(email)) {
-            let scoreEmail = `${score} / ${i}`
-            afficherEmail(nom, email, scoreEmail)
-        } else {
-            console.log(erreur)
-        }
+        let scoreEmail = `${score} / ${i}`
+        gererFormulaire(scoreEmail)
         
     })
 
